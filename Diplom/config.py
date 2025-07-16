8f7c32acbebef73113e957c39cf3da4ef9726033
mov40 <mov40deva@gmail.com>
2025-07-16 15:56:01 +0700

    файлы добавлены

---
+++ Diplom/config.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+# Загружаем переменные среды из .env-файла
+load_dotenv()
+
+# Базовая информация о сайте и API
+LABIRINT_URL = os.getenv('LABIRINT_URL', 'https://www.labirint.ru/')
+API_BASE_URL = os.getenv('LABIRINT_API_URL', 'https://api.labirint.ru/v1/')
+
+# Таймаут ожидания элементов в секундах
+EXPLICIT_WAIT_TIME = int(os.getenv('EXPLICIT_WAIT_TIME', 10)) # Используем for explicit waits in Selenium
+
+# Данные для тестов
+TEST_SEARCH_KIRILLIC = 'Приключения Алисы в Стране Чудес: Льюис Кэрролл'
+TEST_SEARCH_DIGITS = '1000 животных с наклейками'
+TEST_SEARCH_LATIN = 'Harry Potter & the Prisoner of Azkaban: Joanne Rowling'
+TEST_SEARCH_EMPTY = ''
+TEST_SEARCH_STANDARD = 'Война и мир'
+
+# Параметры запросов
+TIMEOUT_REQUEST = int(os.getenv('TIMEOUT_REQUEST', 10))
+REQUEST_RETRIES = int(os.getenv('REQUEST_RETRIES', 3))
+
+# Настройка Selenium (драйвер и опции)
+CHROME_DRIVER_PATH = os.getenv('CHROME_DRIVER_PATH', '/path/to/chromedriver')
---
+++ Diplom/pages/labirint_pages.py
@@ -0,0 +1,73 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from allure_commons.types import AttachmentType
+import allure
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    @allure.step('Открыть страницу {url}')
+    def open(self, url):
+        with allure.step(f'Переход на сайт "{url}"'):
+            self.driver.get(url)
+
+    @allure.step('Ожидание появления элемента {locator}')
+    def wait_until_visible(self, locator):
+        return WebDriverWait(self.driver, EXPLICIT_WAIT_TIME).until(
+            EC.visibility_of_element_located(locator))
+
+    @allure.step('Щёлкнуть по элементу {locator}')
+    def click(self, locator):
+        element = self.wait_until_visible(locator)
+        element.click()
+
+    @allure.step('Набрать текст {text} в поле {locator}')
+    def send_text(self, text, locator):
+        field = self.wait_until_visible(locator)
+        field.clear()
+        field.send_keys(text)
+
+
+class SearchPage(BasePage):
+    INPUT_SEARCH_FIELD = (By.ID, 'search-field')
+    BUTTON_SUBMIT_SEARCH = (By.CLASS_NAME, 'b-header-b-search-e-btn')
+    RESULTS_CONTAINER = (By.CSS_SELECTOR, '.products-row')
+
+    def perform_search(self, keyword):
+        self.send_text(keyword, self.INPUT_SEARCH_FIELD)
+        self.click(self.BUTTON_SUBMIT_SEARCH)
+        return self.check_results_present()
+
+    def check_results_present(self):
+        try:
+            self.wait_until_visible(self.RESULTS_CONTAINER)
+            return True
+        except TimeoutException:
+            return False
+
+
+class GenresPage(BasePage):
+    GENRES_SECTION = (By.CLASS_NAME, 'subgenres')
+    GENRE_LINK_TEMPLATE = (
+        By.XPATH, "//a[contains(@href,'/genres/') and contains(text(),'{}')]")
+
+    def go_to_genres_section(self):
+        section = self.wait_until_visible(self.GENRES_SECTION)
+        return GenresPage(self.driver)
+
+    def choose_genre(self, genre_name):
+        link = self.wait_until_visible(self.GENRE_LINK_TEMPLATE.format(genre_name))
+        link.click()
+        return SearchPage(self.driver)
+
+
+class HomePage(BasePage):
+    HOMEPAGE_LINK = (By.LINK_TEXT, 'Главная')
+
+    def return_to_homepage(self):
+        self.click(self.HOMEPAGE_LINK)
+        return HomePage(self.driver)
+        
